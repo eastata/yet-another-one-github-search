5ae2fb9e31bffd232d8967ae86b81155203220be
Aleksandr Poliakov <aleksandr.poliakov@clearmatics.com>
2020-12-01 15:09:53 +0000

    get repos with pagination

--- main.py
+++ main.py
@@ -19,25 +19,66 @@ def run_query(query_name, header_auth, q_vars):
 
 
 def get_repos(org, header_auth):
+    """
+    :param org: GitHub organisation name
+    :param header_auth: {"Authorization": "Bearer " + github_token} #GitHub Personal Access Token
+    :return: list of repositories available for this organisation
+
+    GitHub has rate limits up to 100 nodes for each requests
+    https://docs.github.com/en/free-pro-team@latest/graphql/overview/resource-limitations#node-limit
+    If number of repos over 100 we must use pagination https://graphql.org/learn/pagination/
+    """
     q_vars = {"q": org}
-    result = run_query('get_repos.graphql', header_auth, q_vars)
-    repos = {}
-    print(result["data"])
-    sys.exit(0)
-    for repo in result["data"]["search"]["edges"]:
-        #print(repo["cursor"])
-        branches = []
-        for branch in repo["node"]["refs"]["edges"]:
-            branches.append(branch["node"]["name"])
-        repos |= {repo["node"]["nameWithOwner"]: branches}
+    result = run_query('get_first_repos.graphql', header_auth, q_vars)
+    repos = []
+    for repo in result["data"]["organization"]["repositories"]["edges"]:
+        repos.append(repo["node"]["name"])
+    while bool(result["data"]["organization"]["repositories"]["pageInfo"]["hasNextPage"]):
+        cursor = result["data"]["organization"]["repositories"]["pageInfo"]["endCursor"]
+        q_vars = {"q": org, "cursor": cursor}
+        result = run_query('get_next_repos.graphql', header_auth, q_vars)
+        for repo in result["data"]["organization"]["repositories"]["edges"]:
+            repos.append(repo["node"]["name"])
     return repos
+
+
+def get_branches(org, header_auth, repo):
+    branches = ["branch1", "branch2"]
+    return branches
+
+
+def get_file_list(org, header_auth, repo, branch, search_dir):
+    file_list = ["./ololo/zz.md", "./ololo/zz2.md"]
+    return file_list
+
+
+def parse_file(org, header_auth, repo, branch, filename, search_pattern):
+    """
+    query {
+      repository(owner: "eastata", name: "zos-cli") {
+        content:object(expression: "master:README.md") {
+          ... on Blob {
+            text
+            isBinary
+          }
+        }
+      }
+    }
+    """
+
+    # Get test blob for file
+    # execute search
+    # Return:
+    # * True if found
+    # * False if not fund
+    pass
 
 
 def main():
     parser = argparse.ArgumentParser(description='Github search by all branches')
     parser.add_argument('--dir',
                         dest='dir',
-                        default="./",
+                        default=".github/workflows/",
                         type=str,
                         help='Dir for search (default: %(default)s)'
                         )
